349b5bc8cbd4d08a0af7ef1308296aae2dba3f46
maxwen <max.weninger@gmail.com>
2018-01-22 10:07:13 -0500

    Trick to make WiFi load on boot
    Change-Id: I508cf23c6424da377b77c7c0c2485141a03bd7ec

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -282,6 +282,7 @@ WIFI_DRIVER_FW_PATH_AP := "ap"
 WIFI_DRIVER_FW_PATH_STA := "sta"
 WIFI_DRIVER_FW_PATH_P2P := "p2p"
 WPA_SUPPLICANT_VERSION := VER_0_8_X
+WIFI_DRIVER_LOAD_DELAY := true
 
 # inherit from the proprietary version
 -include vendor/oneplus/msm8998-common/BoardConfigVendor.mk
